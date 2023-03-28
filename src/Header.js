54a3f9924acf0248aa467818c92d7e2342339400
Andrew Vreeland <andrewvreeland@gmail.com>
2023-03-28 01:47:24 -0400

    updated some css

--- src/Header.js
+++ src/Header.js
@@ -6,9 +6,9 @@ class Header extends React.Component {
   render() {
     return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>My Favorite Books</Navbar.Brand>
         <section>
         <NavItem className='navLink'><Link to="/" className="nav-link"><p>Home</p></Link></NavItem>
+        <Navbar.Brand id='brand'>My Favorite Books</Navbar.Brand>
         <NavItem className='navLink'><Link to="/About" className="nav-link"><p>About us</p></Link></NavItem>
         </section>
       </Navbar>
